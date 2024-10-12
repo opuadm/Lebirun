b9dbf7f0c143a3621120d5d6b882e22ca0e87d14
opuadm <79022132+opuadm@users.noreply.github.com>
2024-10-12 21:51:45 +0200

    Update Kernel.cs

--- LebirunDev/Kernel.cs
+++ LebirunDev/Kernel.cs
@@ -23,8 +23,6 @@ namespace LebirunDev
         public static string CurrentVersion = "0.1.0 Beta 1";
         Canvas canvas;
 
-        // test
-
         protected override void BeforeRun()
         {
             LebirunDev.FileManager.FSLoad();
